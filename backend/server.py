771226448a6d3f221e56aee4b666f5b51056c76f
jbbc-33 <julio.trabajos1505@gmail.com>
2026-06-20 07:34:26 +0200

    arreglado cors render

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import subprocess
 import base64
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.post("/run")
 def run_code():
